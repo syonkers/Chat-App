e71493cc0f1ce2f8c415cb74fb38e8de09949fc9
Shaun Yonkers <syonkers@live.com>
2019-05-22 14:52:30 -0600

    Refactor SeachRoomForm

--- src/Components/Menu/SearchRoomForm.js
+++ src/Components/Menu/SearchRoomForm.js
@@ -2,43 +2,34 @@ import React from 'react';
 import { connect } from'react-redux';
 import toggleSearchRoomForm from '../../Actions/toggleSearchRoomForm';
 
-class SearchRoomForm extends React.Component {
-    constructor(){
-        super();
-        this.handeClick = this.handleClick.bind(this);
-    }
 
-    handleClick(e, socket, dispatch, result) {
-        e.preventDefault();
-        socket.emit('joinChatroom', result);
-        dispatch(toggleSearchRoomForm());
-    }
-
-    render() {
-        const { searchResults, socket, dispatch } = this.props;
-        return (
-        <div className="pop-up-form">
-            <form className="pop-up-form-container">
-                <h1>Room Search</h1>              
-                <div className="search-results">
-                    <ul>
-                    {
-                        searchResults.map(result => {
-                            return(
-                                <li key={result}>
-                                    <button type="button" className="btn-like-link" onClick={(e) => this.handleClick(e, socket, dispatch, result)}>{result}</button>
-                                </li>
-                            );
-                        })
-                    }
-                    </ul>
-                </div>
-                <button type="button" className="btn cancel" onClick={() => dispatch(toggleSearchRoomForm())}>Cancel</button>
-            </form>
-        </div>
-        );
-    }
+const handleClick = (e, socket, dispatch, result) => {
+    e.preventDefault();
+    socket.emit('joinChatroom', result);
+    dispatch(toggleSearchRoomForm());
 }
+
+const SearchRoomForm = ({searchResults, socket, dispatch}) => (
+    <div className="pop-up-form">
+        <form className="pop-up-form-container">
+            <h1>Room Search</h1>              
+            <div className="search-results">
+                <ul>
+                {
+                    searchResults.map(result => {
+                        return(
+                            <li key={result}>
+                                <button type="button" className="btn-like-link" onClick={(e) => handleClick(e, socket, dispatch, result)}>{result}</button>
+                            </li>
+                        );
+                    })
+                }
+                </ul>
+            </div>
+            <button type="button" className="btn cancel" onClick={() => dispatch(toggleSearchRoomForm())}>Cancel</button>
+        </form>
+    </div>
+);
 
 const mapStateToProps = store => ({
     socket: store.socket,
